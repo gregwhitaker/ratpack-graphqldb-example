23ad802d32121e3512abd0dd434755b86d777b90
gregwhitaker <gwhitake@gmail.com>
2018-03-18 09:59:13 -0700

    Doing work

--- src/main/java/ratpack/graphqldb/example/graphql/GraphQLModule.java
+++ src/main/java/ratpack/graphqldb/example/graphql/GraphQLModule.java
@@ -1,11 +1,12 @@
 package ratpack.graphqldb.example.graphql;
 
 import com.google.inject.AbstractModule;
+import ratpack.graphqldb.example.graphql.handler.GraphQLHandler;
 
 public class GraphQLModule extends AbstractModule {
 
     @Override
     protected void configure() {
-
+        bind(GraphQLHandler.class);
     }
 }
