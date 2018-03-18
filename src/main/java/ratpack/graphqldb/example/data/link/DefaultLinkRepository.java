10625cbae9462b32d83fb0c699783e33b3e65589
gregwhitaker <gwhitake@gmail.com>
2018-03-18 11:50:26 -0700

    Doing work

--- src/main/java/ratpack/graphqldb/example/data/link/DefaultLinkRepository.java
+++ src/main/java/ratpack/graphqldb/example/data/link/DefaultLinkRepository.java
@@ -2,9 +2,14 @@ package ratpack.graphqldb.example.data.link;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ratpack.graphqldb.example.data.link.model.Link;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -13,32 +18,58 @@ import java.util.List;
  */
 @Singleton
 public class DefaultLinkRepository implements LinkRepository {
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultLinkRepository.class);
 
     @Inject
     private DataSource dataSource;
 
     @Override
     public List<Link> findAll() {
-        return null;
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement("")) {
+            return null;
+        } catch (SQLException e) {
+            return null;
+        }
     }
 
     @Override
     public Link findOne(long id) {
-        return null;
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement("")) {
+            return null;
+        } catch (SQLException e) {
+            return null;
+        }
     }
 
     @Override
     public void save(Link link) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement("")) {
 
+        } catch (SQLException e) {
+
+        }
     }
 
     @Override
     public Link update(long id, Link link) {
-        return null;
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement("")) {
+            return null;
+        } catch (SQLException e) {
+            return null;
+        }
     }
 
     @Override
     public void delete(long id) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement("")) {
 
+        } catch (SQLException e) {
+
+        }
     }
 }
--- src/main/java/ratpack/graphqldb/example/graphql/GraphQLModule.java
+++ src/main/java/ratpack/graphqldb/example/graphql/GraphQLModule.java
@@ -1,6 +1,10 @@
 package ratpack.graphqldb.example.graphql;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Provides;
+import graphql.schema.idl.RuntimeWiring;
+import ratpack.graphqldb.example.data.link.LinkRepository;
 import ratpack.graphqldb.example.graphql.handler.GraphQLHandler;
 
 public class GraphQLModule extends AbstractModule {
@@ -8,5 +12,11 @@ public class GraphQLModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(GraphQLHandler.class);
+    }
+
+    @Inject
+    @Provides
+    public RuntimeWiring runtimeWiring(LinkRepository linkRepo) {
+        return null;
     }
 }
